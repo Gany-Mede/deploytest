0f9da27c274a62020b51f7b00d82a4eb0069c4f2
Sevastopol06 <vbsevastopol@gmail.com>
2020-09-01 09:25:37 -0700

    PORT

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ server.get("/", (req, res) => {
   res.send("welcome home ");
 });
 
-let PORT = process.env.PORT ? process.env.port : 3000;
+let PORT = process.env.PORT ? process.env.PORT : 3000;
 
 server.listen(PORT, () => {
   console.log("server is listening");
